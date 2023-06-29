829ad2eca12f35738f4f7aa47439993b39b2ac8f
ibra-kdbra <git-repo-develop@outlook.com>
2023-06-29 20:36:48 +0300

    using the assets

---
+++ MarbleMarcher/src/Res.cpp
@@ -0,0 +1,4 @@
+#include "Res.h"
+
+Res::Res(const void* ptr, size_t size)
+  : ptr(ptr), size(size) {}
---
+++ MarbleMarcher/src/Res.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+static const int num_level_music = 4;
+static const char vert_glsl[] = "assets/vert.glsl";
+static const char frag_glsl[] = "assets/frag.glsl";
+static const char Orbitron_Bold_ttf[] = "assets/Orbitron-Bold.ttf";
+static const char Inconsolata_Bold_ttf[] = "assets/Inconsolata-Bold.ttf";
+static const char menu_ogg[] = "assets/menu.ogg";
+static const char level1_ogg[] = "assets/level1.ogg";
+static const char level2_ogg[] = "assets/level2.ogg";
+static const char level3_ogg[] = "assets/level3.ogg";
+static const char level4_ogg[] = "assets/level4.ogg";
+static const char credits_ogg[] = "assets/credits.ogg";
+static const char menu_hover_wav[] = "assets/menu_hover.wav";
+static const char menu_click_wav[] = "assets/menu_click.wav";
+static const char count_down_wav[] = "assets/count_down.wav";
+static const char count_go_wav[] = "assets/count_go.wav";
+static const char arrow_png[] = "assets/arrow.png";
+static const char goal_wav[] = "assets/goal.wav";
+static const char bounce1_wav[] = "assets/bounce1.wav";
+static const char bounce2_wav[] = "assets/bounce2.wav";
+static const char bounce3_wav[] = "assets/bounce3.wav";
+static const char shatter_wav[] = "assets/shatter.wav";
