5da3cdccf1cd69b3a5b9b0051fad4f2c77957b2c
ibra-kdbra <git-repo-develop@outlook.com>
2023-06-30 16:58:43 +0300

    string component

---
+++ miniShell/src/libft/string_fcts/ft_strcmp.c
@@ -0,0 +1,13 @@
+#include "../libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 || *s2)
+	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
