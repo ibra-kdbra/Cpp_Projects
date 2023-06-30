95ad4be678e1143318a487fb59d6c2e49522cd13
ibra-kdbra <git-repo-develop@outlook.com>
2023-06-30 16:46:19 +0300

    calculate with malloc and ft_bzero

---
+++ miniShell/src/libft/memory_fcts/ft_calloc.c
@@ -0,0 +1,16 @@
+#include "../libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*arr;
+
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	if ((size * nmemb) / nmemb != size)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (!arr)
+		return (NULL);
+	ft_bzero(arr, nmemb * size);
+	return (arr);
+}
