bcd75daca21f96e11569139afcc60926e68a9570
ibra-kdbra <git-repo-develop@outlook.com>
2023-06-30 17:05:48 +0300

    string comparing

---
+++ miniShell/src/libft/string_fcts/ft_strncmp.c
@@ -0,0 +1,14 @@
+#include "../libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while ((*s1 || *s2) && n)
+	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+		n--;
+	}
+	return (0);
+}
