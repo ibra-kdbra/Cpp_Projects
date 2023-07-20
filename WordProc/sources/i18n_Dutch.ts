abe3edda2b7e7509ed6004f5583d669f7002845d
ibra-kdbra <git-repo-develop@outlook.com>
2023-07-20 22:29:27 +0300

    languages not supported

---
+++ WordProc/sources/i18n_Dutch.ts
@@ -0,0 +1,333 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>I18N_MAINWINDOW</source>
+        <translation>WordProc</translation>
+    </message>
+    <message>
+        <source>I18N_MM_APP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_EDIT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_WINDOW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_HELP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_SEARCH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_MM_CONFIG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SM_CONFIG_LANG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_NEW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_OPEN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_SAVE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_SAVEAS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_CLOSE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_PRINT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_QUIT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_UNDO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_REDO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_CUT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_COPY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_PASTE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_CLEAR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_SELECTALL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_FIND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_REPLACE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_GOTO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_FORMAT_FONT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_LEFT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_CENTER</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_RIGHT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_NEXT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_PREV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_CLOSE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_CLOSEALL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_HELP_APP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_HELP_QT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_ENGLISH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_DUTCH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_FRENCH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_ITALIAN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_GERMAN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_SPANISH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SM_CONFIG_THEME</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_DEFAULT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_FUSION</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_WINDOWS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_MACOS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_QTCURVE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_DARKMODE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_FILL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_DOCOPEN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_DOCOPEN_FILTER</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_FIND_NEXT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_FIND_PREV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SM_APP_EXPORT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EXPORT_TXT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EXPORT_HTML</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EXPORT_ODF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_EXPORT_PDF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_MAXIMIZE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_MINIMIZE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_DOCSAVE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_DOCSAVE_FILTER</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_TXT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_TXT_FILTER</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_HTML</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_HTML_FILTER</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_ODF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_ODF_FILTER</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_PDF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_PDF_FILTER</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchWindow</name>
+    <message>
+        <source>I18N_SEARCHWINDOW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_FOR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_OPTIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_CASE_SENSITIVE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_WHOLE_WORDS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_CLOSE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_PREV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_NEXT</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TextWindow</name>
+    <message>
+        <source>I18N_TEXTWINDOW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I18N_UNKNOWN_FILE</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ WordProc/sources/i18n_English.ts
@@ -0,0 +1,333 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>I18N_MAINWINDOW</source>
+        <translation>WordProc</translation>
+    </message>
+    <message>
+        <source>I18N_MM_APP</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <source>I18N_MM_EDIT</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <source>I18N_MM_WINDOW</source>
+        <translation>Window</translation>
+    </message>
+    <message>
+        <source>I18N_MM_HELP</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <source>I18N_MM_SEARCH</source>
+        <translation>Search</translation>
+    </message>
+    <message>
+        <source>I18N_MM_CONFIG</source>
+        <translation>Preferences</translation>
+    </message>
+    <message>
+        <source>I18N_SM_CONFIG_LANG</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_NEW</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_OPEN</source>
+        <translation>Open ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_SAVE</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_SAVEAS</source>
+        <translation>Save as ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_CLOSE</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_PRINT</source>
+        <translation>Print</translation>
+    </message>
+    <message>
+        <source>I18N_ME_APP_QUIT</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_UNDO</source>
+        <translation>Undo</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_REDO</source>
+        <translation>Redo</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_CUT</source>
+        <translation>Cut</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_COPY</source>
+        <translation>Copy</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_PASTE</source>
+        <translation>Paste</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_CLEAR</source>
+        <translation>Clear</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EDIT_SELECTALL</source>
+        <translation>Select all</translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_FIND</source>
+        <translation>Find ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_REPLACE</source>
+        <translation>Replace ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_GOTO</source>
+        <translation>Goto ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_FORMAT_FONT</source>
+        <translation>Font</translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_LEFT</source>
+        <translation>Left</translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_CENTER</source>
+        <translation>Center</translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_RIGHT</source>
+        <translation>Right</translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_NEXT</source>
+        <translation>Next</translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_PREV</source>
+        <translation>Previous</translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_CLOSE</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_CLOSEALL</source>
+        <translation>Close all</translation>
+    </message>
+    <message>
+        <source>I18N_ME_HELP_APP</source>
+        <translation>About WordProc ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_HELP_QT</source>
+        <translation>About Qt ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_ENGLISH</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_DUTCH</source>
+        <translation>Dutch</translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_FRENCH</source>
+        <translation>French</translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_ITALIAN</source>
+        <translation>Italian</translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_GERMAN</source>
+        <translation>German</translation>
+    </message>
+    <message>
+        <source>I18N_ME_LANG_SPANISH</source>
+        <translation>Spanish</translation>
+    </message>
+    <message>
+        <source>I18N_SM_CONFIG_THEME</source>
+        <translation>Theme</translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_DEFAULT</source>
+        <translation>Default Theme (Fusion)</translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_FUSION</source>
+        <translation>Fusion</translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_WINDOWS</source>
+        <translation>Windows</translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_MACOS</source>
+        <translation>MacOS</translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_QTCURVE</source>
+        <translation>QtCurve</translation>
+    </message>
+    <message>
+        <source>I18N_ME_THEME_DARKMODE</source>
+        <translation>Dark mode</translation>
+    </message>
+    <message>
+        <source>I18N_ME_ALIGN_FILL</source>
+        <translation>Fill</translation>
+    </message>
+    <message>
+        <source>I18N_DOCOPEN</source>
+        <translation>Open document ...</translation>
+    </message>
+    <message>
+        <source>I18N_DOCOPEN_FILTER</source>
+        <translation>Wordproc Document (*.wpd);; HTML (*.html *.htm);; Text (*.txt *.asc *.ascii *.md *.doc);; All (*)</translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_FIND_NEXT</source>
+        <translation>Find next</translation>
+    </message>
+    <message>
+        <source>I18N_ME_SEARCH_FIND_PREV</source>
+        <translation>Find previous</translation>
+    </message>
+    <message>
+        <source>I18N_SM_APP_EXPORT</source>
+        <translation>Export ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EXPORT_TXT</source>
+        <translation>as TXT ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EXPORT_HTML</source>
+        <translation>as HTML ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EXPORT_ODF</source>
+        <translation>as ODF ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_EXPORT_PDF</source>
+        <translation>as PDF ...</translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_MAXIMIZE</source>
+        <translation>Maximize</translation>
+    </message>
+    <message>
+        <source>I18N_ME_WINDOW_MINIMIZE</source>
+        <translation>Minimize</translation>
+    </message>
+    <message>
+        <source>I18N_DOCSAVE</source>
+        <translation>Save document ...</translation>
+    </message>
+    <message>
+        <source>I18N_DOCSAVE_FILTER</source>
+        <translation>Wordproc Document (*.wpd)</translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_TXT</source>
+        <translation>Export as TXT ...</translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_TXT_FILTER</source>
+        <translation>Wordproc Document (*.txt *.asc *.ascii *.md *.doc)</translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_HTML</source>
+        <translation>Export as HTML ...</translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_HTML_FILTER</source>
+        <translation>Wordproc Document (*.html *.htm)</translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_ODF</source>
+        <translation>Export as ODF ...</translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_ODF_FILTER</source>
+        <translation>Wordproc Document (*.odf)</translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_PDF</source>
+        <translation>Export as PDF ...</translation>
+    </message>
+    <message>
+        <source>I18N_EXPORT_PDF_FILTER</source>
+        <translation>Wordproc Document (*.pdf)</translation>
+    </message>
+</context>
+<context>
+    <name>SearchWindow</name>
+    <message>
+        <source>I18N_SEARCHWINDOW</source>
+        <translation>Search ...</translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_FOR</source>
+        <translation>Search for:</translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_OPTIONS</source>
+        <translation>Options:</translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_CASE_SENSITIVE</source>
+        <translation>Case sensitive</translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_WHOLE_WORDS</source>
+        <translation>Whole parts</translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_CLOSE</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_PREV</source>
+        <translation>Previous</translation>
+    </message>
+    <message>
+        <source>I18N_SEARCH_NEXT</source>
+        <translation>Next</translation>
+    </message>
+</context>
+<context>
+    <name>TextWindow</name>
+    <message>
+        <source>I18N_TEXTWINDOW</source>
+        <translation>TextWindow</translation>
+    </message>
+    <message>
+        <source>I18N_UNKNOWN_FILE</source>
+        <translation>unnamed.wpd</translation>
+    </message>
+</context>
+</TS>
