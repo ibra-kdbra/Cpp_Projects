649d73defc3ac312118c489fc28bc07fdf3e2242
ibra-kdbra <git-repo-develop@outlook.com>
2023-07-22 22:35:48 +0300

    test conan and run it

---
+++ CPP20_Develop/script/test_cmake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e # stop on first error
+
+# default arguments:
+#   script/test_cmake.sh clang debug
+ConfigPreset=${1:-clang}
+Config=${2:-debug}
+BuildPreset="${ConfigPreset}-${Config}"
+TestPreset="test-${BuildPreset}"
+
+BUILD_PATH="build/${ConfigPreset}"
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+if [ ! -f "${BUILD_PATH}/compile_commands.json" ] ; then
+  cmake -S . --preset "${ConfigPreset}"
+fi
+cmake --build --preset "${BuildPreset}"
+ctest --preset "${TestPreset}"
