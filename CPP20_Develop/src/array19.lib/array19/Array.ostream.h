6ccf9192b56117c8301c0f300b0851d2101b8843
ibra-kdbra <git-repo-develop@outlook.com>
2023-07-27 23:57:56 +0300

    array ostream

---
+++ CPP20_Develop/src/array19.lib/array19/Array.ostream.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Array.h"
+
+#include <ostream>
+#include <stddef.h> // size_t
+
+namespace array19 {
+
+template<class Chr, class Traits, class T, size_t C>
+auto operator<<(std::basic_ostream<Chr, Traits>& out, const Array<T, C>& a) -> decltype(out)& {
+    out << "[";
+    bool first = true;
+    for (auto& v : a) {
+        if (first)
+            first = false;
+        else
+            out << ", ";
+        out << v;
+    }
+    return out << "]";
+}
+
+} // namespace array19
