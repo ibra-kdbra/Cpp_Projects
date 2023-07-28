91580b1b7b8a862341f7313550c751993bf039cb
ibra-kdbra <git-repo-develop@outlook.com>
2023-07-28 21:33:07 +1200

    Iterate a container with indices

---
+++ CPP20_Develop/src/array19.lib/array19/WithIndex.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "adlRange.h"
+
+#include <stddef.h> // size_t
+
+namespace array19 {
+
+/// Iterate a container with indices
+/// usage:
+///    for(auto [value, index] : WithIndex{container});
+template<class C> struct WithIndex {
+    using It = decltype(adlBegin(*static_cast<C*>(nullptr)));
+    using Res = decltype(**static_cast<It*>(nullptr));
+
+    // not using std::pair to avoid reference hassles
+    struct result {
+        Res value;
+        size_t index;
+    };
+
+    // minimal iterator that is just enough to run ranged for loop
+    struct iterator {
+        It it;
+        size_t index{};
+
+        [[nodiscard]] constexpr auto operator*() const -> result { return {*it, index}; }
+        [[nodiscard]] constexpr bool operator!=(const It& o) const { return it != o; }
+        constexpr auto operator++() -> iterator& { return (++it, ++index, *this); }
+    };
+
+    constexpr WithIndex(C& c) noexcept : c(c) {}
+
+    [[nodiscard]] constexpr auto begin() -> iterator { return iterator{adlBegin(c), {}}; }
+    [[nodiscard]] constexpr auto end() -> It { return adlEnd(c); }
+
+private:
+    C& c;
+};
+
+template<class C> WithIndex(C&) -> WithIndex<C>;
+
+} // namespace array19
