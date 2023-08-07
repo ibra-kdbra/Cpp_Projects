d10415ca0411a086a2324956de6a857a467a3ea6
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-07 23:19:25 +0300

    ac header

---
+++ design-patterns/command-pattern/ACNotAvailableException.h
@@ -0,0 +1,8 @@
+#ifndef COMMAND_PATTERN_ACNOTAVAILABLEEXCEPTION_H
+#define COMMAND_PATTERN_ACNOTAVAILABLEEXCEPTION_H
+
+#include <exception>
+
+class ACNoteAvailableException : public std::exception {};
+
+#endif //COMMAND_PATTERN_ACNOTAVAILABLEEXCEPTION_H
