8a7ff66fb037296e0dc1c89cf34c5013be9d7efd
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-07 23:20:10 +0300

    not found header

---
+++ design-patterns/command-pattern/CommandNotFound.h
@@ -0,0 +1,8 @@
+#ifndef COMMAND_PATTERN_COMMANDNOTFOUND_H
+#define COMMAND_PATTERN_COMMANDNOTFOUND_H
+
+#include <exception>
+
+class CommandNotFound : public std::exception {};
+
+#endif //COMMAND_PATTERN_COMMANDNOTFOUND_H
