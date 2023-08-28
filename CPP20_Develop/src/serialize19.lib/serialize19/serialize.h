c416ee0251eee29d4e6c5b2efcd0c81aaf3a7580
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-28 23:26:57 +0300

    main serialize header

---
+++ CPP20_Develop/src/serialize19.lib/serialize19/serialize.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Archive.h"
+
+#include <type_traits> // std::is_arithmetic_v, std::is_enum_v
+
+namespace serialize19 {
+
+/// If you hit this fallback you missed a proper overload
+struct SerializeFallback {
+    template<class T> SerializeFallback(const T&) {
+        static_assert(sizeof(T) != sizeof(T), "Missing serialize overload!");
+    }
+};
+template<class A> void serialize(A&, SerializeFallback) {}
+
+template<class T> concept SerializePrimitive = (!std::is_const_v<T> && (std::is_arithmetic_v<T> || std::is_enum_v<T>));
+
+template<Archive A, SerializePrimitive V> auto serialize(A& a, V& v) { a.withPrimitive(v); }
+
+} // namespace serialize19
