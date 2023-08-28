093a3cc974a41250245448e4095c4263dbfa4843
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-28 21:21:49 +1200

    sorted set header

---
+++ CPP20_Develop/src/array19.lib/array19/DynamicSortedSet.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "DynamicArrayOf.h"
+#include "SliceOf.single.h"
+
+#include <algorithm>
+#include <stddef.h> // size_t
+
+namespace array19 {
+
+/// set of sorted of values
+/// - useful for sets of ids
+template<class T, class Less = std::less<>> struct DynamicSortedSet {
+    [[nodiscard]] auto isEmpty() const noexcept -> bool { return m.isEmpty(); }
+    [[nodiscard]] auto count() const -> size_t { return m.count(); }
+    [[nodiscard]] auto begin() const -> T const* { return m.begin(); }
+    [[nodiscard]] auto end() const -> T const* { return m.end(); }
+
+    [[nodiscard]] bool has(const T& v) const {
+        auto [b, e] = std::equal_range(m.begin(), m.end(), v, Less{});
+        return b != e;
+    }
+
+    void add(T const& v) {
+        auto [b, e] = std::equal_range(m.amendBegin(), m.amendEnd(), v, Less{});
+        if (b == e) {
+            m.splice(b, 0, array19::sliceOfSingle(v));
+        }
+    }
+
+    void remove(T const& v) {
+        auto [b, e] = std::equal_range(m.amendBegin(), m.amendEnd(), v, Less{});
+        if (b != e) {
+            m.remove(b, 1);
+        }
+    }
+
+    void clear() { m.clear(); }
+
+private:
+    DynamicArrayOf<T> m;
+};
+
+} // namespace array19
