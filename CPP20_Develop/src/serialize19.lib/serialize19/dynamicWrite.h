2ce2bd8909fe0603f11011551820b465dc4f329a
ibra-kdbra <git-repo-develop@outlook.com>
2023-08-28 23:20:49 +0300

    dynamic writing header

---
+++ CPP20_Develop/src/serialize19.lib/serialize19/dynamicWrite.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "SizeArchive.h"
+#include "UniqueBuffer.h"
+#include "WriteToArchive.h"
+
+namespace serialize19 {
+
+/// serializes T to a dynamically allocated UniqueBuffer
+template<EndianBehaviour endian = EndianBehaviour::Keep, class T> auto dynamicWrite(const T& value) -> UniqueBuffer {
+    auto sizeArchive = SizeArchive{};
+    serialize(sizeArchive, value);
+    auto buffer = UniqueBuffer{sizeArchive.size()};
+    auto writeArchive = WriteToArchive<endian>{buffer.amendSlice()};
+    serialize(writeArchive, value);
+    return buffer;
+}
+
+} // namespace serialize19
