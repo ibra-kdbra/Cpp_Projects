6f2b5493c7c3bdd347648f87c5685a86de9c3414
ibra-kdbra <git-repo-develop@outlook.com>
2023-09-04 23:51:05 +0300

    test rope main

---
+++ CPP20_Develop/src/string19.lib/string19/Rope.decimal.test.cpp
@@ -0,0 +1,30 @@
+#include "Rope.Decimal.h"
+#include "Rope.Rope.h"
+#include "Rope.StringView.h"
+#include "Rope.store.h"
+#include "StringStore.literal.h"
+#include "StringView.equals.h"
+#include "StringView.literal.h"
+#include "StringView.store.h"
+
+#include <gtest/gtest.h>
+
+using namespace string19;
+
+TEST(Rope_decimal, example) {
+    constexpr auto rope = Rope{viewLiteral("dec: "), decimal<13>};
+    constexpr auto N = ropeCount(rope);
+    constexpr auto store = ropeStore<N>(rope);
+
+    static_assert(viewStore(store) == viewLiteral("dec: 13"));
+    EXPECT_EQ(store, storeLiteral("dec: 13"));
+}
+
+TEST(Rope_decimal, negative) {
+    constexpr auto rope = Rope{viewLiteral("dec: "), decimal<-13>};
+    constexpr auto N = ropeCount(rope);
+    constexpr auto store = ropeStore<N>(rope);
+
+    static_assert(viewStore(store) == viewLiteral("dec: -13"));
+    EXPECT_EQ(store, storeLiteral("dec: -13"));
+}
