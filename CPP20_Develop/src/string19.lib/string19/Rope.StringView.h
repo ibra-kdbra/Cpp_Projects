4849785024e3b2c7f18c475bd45df7990bb4a5c4
ibra-kdbra <git-repo-develop@outlook.com>
2023-09-06 21:05:23 +0300

    rope string auto ropeCount

---
+++ CPP20_Develop/src/string19.lib/string19/Rope.StringView.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "ADL.h"
+#include "StringView.h"
+
+#include <stddef.h> // size_t
+
+namespace string19 {
+
+constexpr auto ropeCount(ADL*, StringView sv) -> size_t { return sv.count; }
+
+constexpr void ropeAppend(ADL*, char*& data, StringView sv) {
+    for (auto i = 0u; i < sv.count; ++i) *data++ = sv[i];
+}
+
+} // namespace string19
