81ca2174a841a948ac07a4bb4234e20db865678f
ibra-kdbra <git-repo-develop@outlook.com>
2023-09-09 23:43:14 +0300

    helper for argument dependent lookup

---
+++ CPP20_Develop/src/strong19.lib/strong19/ADL.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "meta19/nullptr_to.h"
+
+namespace strong19 {
+
+using meta19::nullptr_to;
+
+// helper for argument dependent lookup (ADL)
+// used to find function overloads in this namespace
+struct ADL;
+
+} // namespace strong19
