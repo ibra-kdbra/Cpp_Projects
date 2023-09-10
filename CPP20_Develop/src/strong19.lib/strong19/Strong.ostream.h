5a7790c33a01a78f53019aa40a3cbd422f03afb0
ibra-kdbra <git-repo-develop@outlook.com>
2023-09-10 18:16:23 +0300

    strong output stream

---
+++ CPP20_Develop/src/strong19.lib/strong19/Strong.ostream.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Strong.h"
+#include "string19/StringView.ostream.h"
+
+#include <ostream>
+
+namespace strong19 {
+
+template<class T>
+concept Ostreamable = requires(T v, std::ostream& out) {
+                          { out << v.v };
+                      };
+
+template<class S> auto strongOstream(std::ostream& out, const S& s) -> std::ostream& {
+    if constexpr (Ostreamable<S>) {
+        return out << strong_name<S> << '{' << s.v << '}';
+    }
+    else {
+        return out << strong_name<S> << "{<unprintable>}";
+    }
+}
+
+/// defines an ostream operator for given strong type (also prints name of the strong type)
+#define DEFINE_STRONG_OSTREAM(NAME)                                                                                    \
+    inline auto operator<<(std::ostream& out, const NAME& s)->std::ostream& { return strong19::strongOstream(out, s); }
+
+} // namespace strong19
