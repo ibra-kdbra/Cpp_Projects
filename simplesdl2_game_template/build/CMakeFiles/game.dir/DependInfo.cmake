b02f64a599b674f49d81a6f9b2eb5ae6dafacf97
ibra-kdbra <git-repo-develop@outlook.com>
2023-10-01 22:36:22 +0300

    build to start the game

---
+++ simplesdl2_game_template/build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ibra-kdbra/Workspace/tryOut/simplesdl2_game_template/src/gamehandler.cpp" "CMakeFiles/game.dir/src/gamehandler.cpp.o" "gcc" "CMakeFiles/game.dir/src/gamehandler.cpp.o.d"
+  "/home/ibra-kdbra/Workspace/tryOut/simplesdl2_game_template/src/gamemanager.cpp" "CMakeFiles/game.dir/src/gamemanager.cpp.o" "gcc" "CMakeFiles/game.dir/src/gamemanager.cpp.o.d"
+  "/home/ibra-kdbra/Workspace/tryOut/simplesdl2_game_template/src/input.cpp" "CMakeFiles/game.dir/src/input.cpp.o" "gcc" "CMakeFiles/game.dir/src/input.cpp.o.d"
+  "/home/ibra-kdbra/Workspace/tryOut/simplesdl2_game_template/src/main.cpp" "CMakeFiles/game.dir/src/main.cpp.o" "gcc" "CMakeFiles/game.dir/src/main.cpp.o.d"
+  "/home/ibra-kdbra/Workspace/tryOut/simplesdl2_game_template/src/object.cpp" "CMakeFiles/game.dir/src/object.cpp.o" "gcc" "CMakeFiles/game.dir/src/object.cpp.o.d"
+  "/home/ibra-kdbra/Workspace/tryOut/simplesdl2_game_template/src/tasker.cpp" "CMakeFiles/game.dir/src/tasker.cpp.o" "gcc" "CMakeFiles/game.dir/src/tasker.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simplesdl2_game_template/build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/src/gamehandler.cpp.o"
+  "CMakeFiles/game.dir/src/gamehandler.cpp.o.d"
+  "CMakeFiles/game.dir/src/gamemanager.cpp.o"
+  "CMakeFiles/game.dir/src/gamemanager.cpp.o.d"
+  "CMakeFiles/game.dir/src/input.cpp.o"
+  "CMakeFiles/game.dir/src/input.cpp.o.d"
+  "CMakeFiles/game.dir/src/main.cpp.o"
+  "CMakeFiles/game.dir/src/main.cpp.o.d"
+  "CMakeFiles/game.dir/src/object.cpp.o"
+  "CMakeFiles/game.dir/src/object.cpp.o.d"
+  "CMakeFiles/game.dir/src/tasker.cpp.o"
+  "CMakeFiles/game.dir/src/tasker.cpp.o.d"
+  "game"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
