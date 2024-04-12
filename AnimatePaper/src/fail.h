dc562f44f6de0fecc14d471fd85e8939342b0bb9
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-13 01:21:49 +0300

    delete outdated features

--- AnimatePaper/src/fail.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void MessageAndQuit(int line, const char *file, const char *message, ...);
-
-#define FAIL_WITH(M, ...) MessageAndQuit(__LINE__, __FILE__, M, ##__VA_ARGS__)
-#define FAIL() MessageAndQuit(__LINE__, __FILE__, NULL)
--- AnimatePaper/src/video.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct Context;
-
-struct Video* VideoLoad(const struct Context *context);
-void VideoUpdate(double delta_sec, struct Video *video, const struct Context *context);
-void VideoClear(struct Video *video, const struct Context *context);
