eeca850712e55b650a7b7bd011a0bc6ec30913c0
ibra-kdbra <git-repo-develop@outlook.com>
2024-08-05 07:27:44 +0300

    fcfg server cfg free config array

--- ViteFA_config/src/server/fcfg_server_cfg.c
+++ ViteFA_config/src/server/fcfg_server_cfg.c
@@ -24,4 +24,10 @@ static int64_t current_config_version = 0;
 
 static struct fast_mblock_man event_allocator;
 
+static int fcfg_server_cfg_free_config_array(void *args)
+{
+    fcfg_server_dao_free_config_array((FCFGConfigArray *)args);
+    free(args);
+    return 0;
+}
 
