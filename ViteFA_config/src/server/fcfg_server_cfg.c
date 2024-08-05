a619e2b85c30cbd9f1f713ef66084a2cff090cdb
ibra-kdbra <git-repo-develop@outlook.com>
2024-08-05 07:28:27 +0300

    fcfg check alloc config array

--- ViteFA_config/src/server/fcfg_server_cfg.c
+++ ViteFA_config/src/server/fcfg_server_cfg.c
@@ -31,3 +31,49 @@ static int fcfg_server_cfg_free_config_array(void *args)
     return 0;
 }
 
+static int check_alloc_config_array(FCFGConfigArray **array, const int inc)
+{
+    FCFGConfigArray *old_array;
+    FCFGConfigArray *new_array;
+    int target_count;
+    int result;
+    int bytes;
+
+    target_count = (*array)->count + inc;
+    if ((*array)->alloc >= target_count) {
+        return 0;
+    }
+
+    new_array = (FCFGConfigArray *)malloc(sizeof(FCFGConfigArray));
+    if (new_array == NULL) {
+        logError("file: "__FILE__", line: %d, "
+                "malloc %d bytes fail",
+                __LINE__, (int)sizeof(FCFGConfigArray));
+        return ENOMEM;
+    }
+
+    new_array->alloc = ((*array)->alloc > 0) ? (*array)->alloc : 8;
+    while (new_array->alloc < target_count) {
+        new_array->alloc *= 2;
+    }
+
+    bytes = sizeof(FCFGConfigEntry) * new_array->alloc;
+    new_array->rows = (FCFGConfigEntry *)malloc(bytes);
+    if (new_array->rows == NULL) {
+        logError("file: "__FILE__", line: %d, "
+                "malloc %d bytes fail", __LINE__, bytes);
+        return ENOMEM;
+    }
+
+    new_array->count = 0;
+    new_array->version = __sync_add_and_fetch(&current_config_version, 1);
+    if ((result=fcfg_server_dao_copy_config_array(*array, new_array)) != 0) {
+        return result;
+    }
+
+    old_array = *array;
+    *array = new_array;
+    return sched_add_delay_task(fcfg_server_cfg_free_config_array,
+            old_array, 10 * SF_G_NETWORK_TIMEOUT, false);
+}
+
