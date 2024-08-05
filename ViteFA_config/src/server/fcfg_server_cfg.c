4ed9045d143b683c1a934f4206f2f91f04f22279
ibra-kdbra <git-repo-develop@outlook.com>
2024-08-05 07:24:31 +0300

    init the type struct

---
+++ ViteFA_config/src/server/fcfg_server_cfg.c
@@ -0,0 +1,27 @@
+
+#include <sys/stat.h>
+#include "fastcommon/shared_func.h"
+#include "fastcommon/logger.h"
+#include "fastcommon/pthread_func.h"
+#include "fastcommon/sched_thread.h"
+#include "fastcommon/fc_list.h"
+#include "sf/sf_global.h"
+#include "common/fcfg_types.h"
+#include "fcfg_server_global.h"
+#include "fcfg_server_dao.h"
+#include "fcfg_server_cfg.h"
+
+typedef struct {
+    FCFGEnvPublisher **envs;
+    int count;
+    int alloc;
+    pthread_mutex_t lock;
+} FCFGPublisherArray;
+
+static FCFGPublisherArray publisher_array = {NULL, 0, 0};
+
+static int64_t current_config_version = 0;
+
+static struct fast_mblock_man event_allocator;
+
+
