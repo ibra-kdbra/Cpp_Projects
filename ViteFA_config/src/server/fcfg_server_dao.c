5723a9f960421b53167d8d5a16f380e3a097ea1b
ibra-kdbra <git-repo-develop@outlook.com>
2024-08-14 23:55:13 +0300

    some main defines to use

--- ViteFA_config/src/server/fcfg_server_dao.c
+++ ViteFA_config/src/server/fcfg_server_dao.c
@@ -47,3 +47,53 @@ static int fcfg_server_dao_check_stmt(FCFGMySQLContext *context, MYSQL_STMT **st
     return 0;
 }
 
+#define CONFIG_SELECT_SQL "SELECT name, type, value, version, status, " \
+        "UNIX_TIMESTAMP(create_time), UNIX_TIMESTAMP(update_time) FROM fast_config "
+
+const char *insert_sql = "INSERT INTO fast_config "
+    "(env, name, type, value, version, status) VALUES (?, ?, ?, ?, ?, 0)";
+const char *update_sql = "UPDATE fast_config "
+    "SET type = ?, value = ?, version = ?, status = 0 WHERE env = ? AND name = ?";
+const char *delete_sql = "UPDATE fast_config "
+    "SET version = ?, status = 1 WHERE env = ? AND name = ? and status = 0";
+const char *select_sql = CONFIG_SELECT_SQL
+    "WHERE env = ? AND version > ? ORDER BY version limit ?";
+const char *search_sql = CONFIG_SELECT_SQL
+    "WHERE env = ? AND name like ? and status = 0 ORDER BY name limit ?, ?";
+const char *get_pk_sql = CONFIG_SELECT_SQL
+    "WHERE env = ? AND name = ? and status = 0";
+const char *max_env_ver_sql = "SELECT MAX(version) FROM fast_environment";
+const char *max_cfg_ver_sql = "SELECT MAX(version) FROM fast_config "
+    "WHERE env = ?";
+
+#define FCFG_GET_ADMIN_UPDATE_STMT(context) \
+            fcfg_server_dao_check_stmt(context, &context->admin.update_stmt, \
+                    update_sql)
+
+#define FCFG_GET_ADMIN_INSERT_STMT(context) \
+            fcfg_server_dao_check_stmt(context, &context->admin.insert_stmt, \
+                    insert_sql)
+
+#define FCFG_GET_ADMIN_DELETE_STMT(context) \
+            fcfg_server_dao_check_stmt(context, &context->admin.delete_stmt, \
+                    delete_sql)
+
+#define FCFG_GET_ADMIN_SEARCH_STMT(context) \
+            fcfg_server_dao_check_stmt(context, &context->admin.search_stmt, \
+                    search_sql)
+
+#define FCFG_GET_ADMIN_GET_PK_STMT(context) \
+            fcfg_server_dao_check_stmt(context, &context->admin.get_pk_stmt, \
+                    get_pk_sql)
+
+#define FCFG_GET_AGENT_SELECT_STMT(context) \
+            fcfg_server_dao_check_stmt(context, &context->agent.select_stmt, \
+                    select_sql)
+
+#define FCFG_GET_MONITOR_MAX_ENV_VER_STMT(context) \
+            fcfg_server_dao_check_stmt(context, &context->monitor.max_env_ver_stmt, \
+                    max_env_ver_sql)
+
+#define FCFG_GET_MONITOR_MAX_CFG_VER_STMT(context) \
+            fcfg_server_dao_check_stmt(context, &context->monitor.max_cfg_ver_stmt, \
+                    max_cfg_ver_sql)
