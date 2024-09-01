185b9616d8ec758182734e83027ac3c18ad940a5
ibra-kdbra <git-repo-develop@outlook.com>
2024-09-02 00:01:41 +0300

    xhtml basic

---
+++ staticpagegen/xhtmlbasic1-0.c
@@ -0,0 +1,18 @@
+#include "xhtmlbasic1-0.h"
+
+char xhtmlbasic10top[] = "<?xml version=\"1.0\"?>\n\
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML Basic 1.0//EN\"\n\
+    \"http://www.w3.org/TR/xhtml-basic/xhtml-basic10.dtd\">\n\
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">\n\
+  <head>\n\
+    <title>";
+char xhtmlbasic10mid1[] = "</title>\n\
+  </head>\n\
+  <body>\n\
+    <h1><a href=\"/\">";
+char xhtmlbasic10mid2[] = "</a></h1>\n\
+    <pre>\n";
+char xhtmlbasic10bot[] = "\
+    </pre>\n\
+  </body>\n\
+</html>\n";
---
+++ staticpagegen/xhtmlbasic1-0.h
@@ -0,0 +1,7 @@
+#ifndef _STATICPAGEGEN_XHTMLBASIC_1_0_H_
+#define _STATICPAGEGEN_XHTMLBASIC_1_0_H_
+extern char xhtmlbasic10top[];
+extern char xhtmlbasic10mid1[];
+extern char xhtmlbasic10mid2[];
+extern char xhtmlbasic10bot[];
+#endif /* _STATICPAGEGEN_XHTMLBASIC_1_0_H_ */
