2588a7519d1ff073ec876fb9e5c88a3dcde659a1
ibra-kdbra <git-repo-develop@outlook.com>
2024-12-01 23:34:58 +0300

    added french, japenese

---
+++ GenXPassword/i18n/es.ts
@@ -0,0 +1,295 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es" sourcelanguage="en">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="65"/>
+        <source>GenPassword is now running</source>
+        <translation>GenPassword ya está en marcha</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="66"/>
+        <source>The application is already running.
+Another instance cannot be started.</source>
+        <translation>La aplicación ya se está ejecutando.
+No se puede iniciar otra instancia.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="78"/>
+        <source>Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="79"/>
+        <source>The theme index specified in the configuration file is invalid. 
+Please check the value and try again.</source>
+        <translation>El índice del tema especificado en el archivo de configuración no es válido. 
+Compruebe el valor e inténtelo de nuevo.</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="39"/>
+        <source>Numeric characters</source>
+        <translation>Caracteres numéricos</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="40"/>
+        <source>Symbols characters</source>
+        <translation>Símbolos especiales</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="41"/>
+        <source>Lowercase letters</source>
+        <translation>Letras minúsculas</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="42"/>
+        <source>Uppercase letters</source>
+        <translation>Letras mayúsculas</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="52"/>
+        <source>Your password will be generated here</source>
+        <translation>Su contraseña se generará aquí</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="60"/>
+        <source>Generate a new password</source>
+        <translation>Generar una nueva contraseña</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="63"/>
+        <source>Copy the password to the clipboard</source>
+        <translation>Copiar la contraseña en el portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="72"/>
+        <source>Contain the numbers: 0-9</source>
+        <translation>Contiene los números: 0-9</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="76"/>
+        <source>Contain special symbols:
+!&quot;#$%&amp;&apos;()*+,-./:;&lt;=&gt;?@[\]^_`{|}~</source>
+        <translation>Contiene símbolos especiales:
+!&quot;#$%&amp;&apos;()*+,-./:;&lt;=&gt;?@[\]^_`{|}~</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="80"/>
+        <source>Contain lowercase letters: a-z</source>
+        <translation>Contiene letras minúsculas: a-z</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="84"/>
+        <source>Contain uppercase letters: A-Z</source>
+        <translation>Contiene letras mayúsculas: A-Z</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="86"/>
+        <source>Characters to add</source>
+        <translation>Caracteres a añadir</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="89"/>
+        <source>Characters to remove</source>
+        <translation>Caracteres a eliminar</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="119"/>
+        <source>Add</source>
+        <translation>Añadir</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="120"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="257"/>
+        <source>Information</source>
+        <translation>Información</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="258"/>
+        <source>No characters have been selected for the password. 
+Please select at least one character.</source>
+        <translation>No se ha seleccionado ningún carácter para la contraseña. 
+Por favor, seleccione al menos un carácter.</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="268"/>
+        <source>Hide Password</source>
+        <translation>Ocultar contraseña</translation>
+    </message>
+    <message>
+        <location filename="../passwordgenerator.cpp" line="272"/>
+        <source>Show Password</source>
+        <translation>Mostrar contraseña</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../window.cpp" line="150"/>
+        <source>&amp;Languages</source>
+        <translation>Idiomas</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="151"/>
+        <source>&amp;Themes</source>
+        <translation>&amp;Temas</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="152"/>
+        <source>&amp;Shortcuts</source>
+        <translation>Atajos</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="153"/>
+        <source>&amp;Help</source>
+        <translation>Ayuda</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="158"/>
+        <source>[en] English</source>
+        <translation>[en] Inglés</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="159"/>
+        <source>[es] Español</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="161"/>
+        <source>Light</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="162"/>
+        <source>Dark</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="168"/>
+        <source>&amp;Custom Title Bar</source>
+        <translation>&amp;Barra de título personalizada</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="170"/>
+        <source>&amp;About %1</source>
+        <translation>&amp;Acerca de %1</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="171"/>
+        <source>&amp;Credits</source>
+        <translation>&amp;Créditos</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="172"/>
+        <source>About &amp;Qt</source>
+        <translation>Acerca de &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="174"/>
+        <source>E&amp;xit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="182"/>
+        <source>&amp;Generate Password</source>
+        <translation>&amp;Generar contraseña</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="185"/>
+        <source>Cut Password</source>
+        <translation>Cortar contraseña</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="189"/>
+        <source>&amp;Copy Password</source>
+        <translation>&amp;Copiar contraseña</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="257"/>
+        <source>Show %1</source>
+        <translation>Mostrar %1</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="258"/>
+        <source>New+Copy Password</source>
+        <translation>Nueva+Copiar contraseña</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="350"/>
+        <source>Restart Required</source>
+        <translation>Reinicio necesario</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="351"/>
+        <source>You will need to restart in order to use your new language setting
+Your password will be lost if you do not save it before restarting.</source>
+        <translation>Deberá reiniciar para poder utilizar la nueva configuración de idioma.
+Su contraseña se perderá si no la guarda antes de reiniciar.</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="355"/>
+        <source>Restart Now</source>
+        <translation>Reiniciar ahora</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="357"/>
+        <source>Restart Later</source>
+        <translation>Reiniciar más tarde</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="370"/>
+        <source>Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="371"/>
+        <source>Error: &quot;enum QProcess::ProcessError&quot;
+Value error: %1
+The program can still be used
+but cannot be restarted due to an error.
+
+Do you want copy this error in your clipboad?</source>
+        <translation>Error: &quot;enum QProcess::ProcessError&quot;
+Error de valor: %1
+El programa todavía se puede utilizar
+pero no se puede reiniciar debido a un error.
+
+¿Quieres copiar este error en tu portapapeles?</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="377"/>
+        <source>Copy to clipboard</source>
+        <translation>Copiar al portapapeles</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="379"/>
+        <source>No</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="415"/>
+        <source>&lt;div style=&quot;text-align: center&quot;&gt;&lt;h1&gt;%1&lt;/h1&gt;&lt;p&gt;Version: %2&lt;/p&gt;&lt;p&gt;%1 is a free password generator that helps you create random and customizable passwords. The program adds or removes characters in the following order:&lt;/p&gt;&lt;ol&gt;&lt;li&gt;Numbers, Symbols, Lowercase letters, Uppercase letters&lt;/li&gt;&lt;li&gt;Adds the letters that the user has specified&lt;/li&gt;&lt;li&gt;Removes the letters that the user has specified&lt;/li&gt;&lt;/ol&gt;&lt;p&gt;For issues or contributions, visit the &lt;a href=https://github.com/ibra-kdbra/Cpp_Projects/tree/main/GenXPassword style=color:#0097CF&gt;GitHub repository&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Copyright © 2023 - ibra-kdbra&lt;/p&gt;&lt;p&gt;This program comes with NO WARRANTY OF ANY KIND.&lt;br&gt;See the &lt;a href=https://www.gnu.org/licenses/gpl-3.0.html style=color:#0097CF&gt;GNU General Public License&lt;/a&gt; for details.&lt;/p&gt;&lt;/div&gt;</source>
+        <translation>&lt;div style=&quot;text-align: center&quot;&gt;&lt;h1&gt;%1&lt;/h1&gt;&lt;p&gt;Versión: %2&lt;/p&gt;&lt;p&gt;%1 es un generador de contraseñas gratuito que te ayuda a crear contraseñas aleatorias y personalizables. El programa añade o elimina caracteres en el siguiente orden:&lt;/p&gt;&lt;ol&gt;&lt;li&gt;Números, Símbolos, Minúsculas, Mayúsculas&lt;/li&gt;&lt;li&gt;Añade las letras que el usuario ha especificado&lt;/li&gt;&lt;li&gt;Elimina las letras que el usuario ha especificado&lt;/li&gt;&lt;/ol&gt;&lt;p&gt;Para cuestiones o contribuciones, visite &lt;a href=https://github.com/ibra-kdbra/Cpp_Projects/tree/main/GenXPassword style=color:#0097CF&gt;GitHub repository&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Copyright © 2023 - ibra-kdbra&lt;/p&gt;&lt;p&gt;Este programa no incluye GARANTÍA DE NINGÚN TIPO.&lt;br&gt;Véase el &lt;a href=https://www.gnu.org/licenses/gpl-3.0.html style=color:#0097CF&gt;GNU General Public License&lt;/a&gt; para más detalles.&lt;/p&gt;&lt;/div&gt;</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="436"/>
+        <source>About %1</source>
+        <translation>Acerca de %1</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="442"/>
+        <source>&lt;div style=&quot;text-align: center&quot;&gt;&lt;h1&gt;%1&lt;/h1&gt;&lt;p&gt;Created by &lt;a href=https://github.com/ibra-kdbra style=color:#0097CF&gt;ibra-kdbra&lt;/a&gt;&lt;/p&gt;&lt;p&gt;This password generator provides customizable options for creating secure passwords. Feel free to use and share it under the GNU General Public License v3.0.&lt;/p&gt;&lt;/div&gt;</source>
+        <translation>&lt;div style=&quot;text-align: center&quot;&gt;&lt;h1&gt;%1&lt;/h1&gt;&lt;p&gt;Creado por &lt;a href=https://github.com/ibra-kdbra style=color:#0097CF&gt;ibra-kdbra&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Este generador de contraseñas ofrece opciones personalizables para crear contraseñas seguras. Siéntase libre de usarlo y compartirlo bajo la Licencia Pública General GNU v3.0.&lt;/p&gt;&lt;/div&gt;</translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="455"/>
+        <source>Credits</source>
+        <translation>Créditos</translation>
+    </message>
+</context>
+</TS>
