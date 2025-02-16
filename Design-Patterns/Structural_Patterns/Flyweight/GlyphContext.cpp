c73d9f22d33dbb312d3c9d33a7f55744b17f569a
ibra-kdbra <git-repo-develop@outlook.com>
2025-02-17 00:00:48 +0300

    glyph context some steps done

---
+++ Design-Patterns/Structural_Patterns/Flyweight/GlyphContext.cpp
@@ -0,0 +1,34 @@
+// TODO: implement these.
+#include "GlyphContext.h"
+
+GlyphContext::GlyphContext()
+{}
+
+GlyphContext::~GlyphContext()
+{}
+
+void GlyphContext::Next(int step)
+{
+    // Increment _index as the traversal proceeds (see book page 202).
+    _index += step;
+}
+
+void GlyphContext::Insert(int quantity1)
+{}
+
+Font* GlyphContext::GetFont()
+{
+    // TODO: GlyphContext::GetFont() uses the index as a key into a BTree
+    // structure that stores the glyph-to-font mapping.  Each node in the tree
+    // is labeled with the length of the string for which it gives font
+    // information.  Leaves in the tree point to a font, while interior nodes
+    // break the string into substrings, one for each child (see page 202-203
+    // in the book).
+    return 0;
+}
+
+void GlyphContext::SetFont(Font*, int span)
+{}
+
+//    int _index;
+//    BTree* _fonts;
